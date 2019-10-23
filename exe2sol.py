970a7e10c0e0a5942f15f738e73f74d799e887ec
giseldo <giseldo@gmail.com>
2019-10-23 18:18:33 -0300

    respostas

--- exe2sol.py
+++ exe2sol.py
@@ -1,8 +1,8 @@
 # https://campus.datacamp.com/courses/building-chatbots-in-python/chatbots-101?ex=3
 # EchoBot II
 # Having written your respond() function, you'll now define a function called send_message() with a single parameter message which logs the message and the bot's response.
- 
-# Instructions   
+
+# Instructions
 # Use the user_template string's .format() method to include the user's message into the user template, and print the result.
 # Call the respond() function with the message passed in and save the result as response.
 # Log the bot's response using the bot_template string's .format() method.
@@ -19,13 +19,14 @@ def respond(message):
     return bot_message
 
 # Define a function that sends a message to the bot: send_message
-def ____(____):
+def send_message(message):
     # Print user_template including the user_message
-    print(____.format(____))
+    print(user_template.format(message))
     # Get the bot's response to the message
-    response = ____(____)
+    response = respond(message)
     # Print the bot template including the bot's response.
-    print(____.format(____))
+    print(bot_template.format(response))
+
 
 # Send a message to the bot
-send_message("____")
+send_message("hello")
