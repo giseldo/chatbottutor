1d3cce0ad14bc824e10a57290d76a8f7d9cddd35
Giseldo <giseldo@gmail.com>
2019-10-23 18:04:56 -0300

    Create exe2.py

---
+++ exe2.py
@@ -0,0 +1,9 @@
+
+EchoBot II
+Having written your respond() function, you'll now define a function called send_message() with a single parameter message which logs the message and the bot's response.
+    
+    
+    Use the user_template string's .format() method to include the user's message into the user template, and print the result.
+    Call the respond() function with the message passed in and save the result as response.
+    Log the bot's response using the bot_template string's .format() method.
+    Send the message "hello" to the bot.
