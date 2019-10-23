4b48be53e75bbb35a09cdca11d31aec1bec70647
Giseldo <giseldo@gmail.com>
2019-10-23 17:55:50 -0300

    Create exe1.py

---
+++ exe1.py
@@ -0,0 +1,12 @@
+bot_template = "BOT : {0}"
+user_template = "USER : {0}"
+
+# Define a function that responds to a user's message: respond
+def ____(____):
+    # Concatenate the user's message to the end of a standard bot respone
+    bot_message = "____" + ____
+    # Return the result
+    return ____
+
+# Test function
+print(respond("hello!"))
