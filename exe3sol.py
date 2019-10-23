0ae52c3af357009a1a4dd5023d9e169fd7d1d68f
giseldo <giseldo@gmail.com>
2019-10-23 19:14:30 -0300

    adicionando o exercicio 3

--- exe3sol.py
+++ exe3sol.py
@@ -1,12 +1,15 @@
 # https://campus.datacamp.com/courses/building-chatbots-in-python/chatbots-101?ex=5
 # Chitchat
-# Now you're going to leave the simple EchoBot behind and create a bot which can answer simple questions such as "What's your name?" and "What's today's weather?"
+# Now you're going to leave the simple EchoBot behind and create a bot which can answer simple questions
+# such as "What's your name?" and "What's today's weather?"
 # You'll use a dictionary with these questions as keys and the correct responses as values.
-# This means the bot will only respond correctly if the message matches exactly, which is a big limitation. In later exercises you will create much more robust solutions.
+# This means the bot will only respond correctly if the message matches exactly, which is a big limitation.
+# In later exercises you will create much more robust solutions.
 
 # Instructions 1/2
 # The send_message() function has already been defined for you, as well as the bot_template and user_template variables.
-# Define a respond() function which takes in a message argument, checks if the message has a pre-defined response, and returns the response in the responses dictionary if there is a match, or the "default" message otherwise.
+# Define a respond() function which takes in a message argument, checks if the message has a pre-defined response,
+# and returns the response in the responses dictionary if there is a match, or the "default" message otherwise.
 
 
 # Create templates
@@ -25,14 +28,14 @@ responses = {
 }
 
 # Return the matching response if there is one, default otherwise
-def ____(____):
+def respond(message):
     # Check if the message is in the responses
-    if ____ in ____:
+    if message in responses:
         # Return the matching message
-        bot_message = ____[____]
+        bot_message = responses[message]
     else:
         # Return the "default" message
-        bot_message = ____["____"]
+        bot_message = responses["default"]
     return bot_message
 
 # Define a function that sends a message to the bot: send_message
