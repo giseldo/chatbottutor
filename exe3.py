f4cfacd908c484ca6cf5fb52dc9baa85f132fab8
Giseldo <giseldo@gmail.com>
2019-10-23 18:19:42 -0300

    Create exe3.py

---
+++ exe3.py
@@ -0,0 +1,9 @@
+Chitchat
+
+Now you're going to leave the simple EchoBot behind and create a bot which can answer simple questions such as "What's your name?" and "What's today's weather?"
+
+You'll use a dictionary with these questions as keys and the correct responses as values.
+
+This means the bot will only respond correctly if the message matches exactly, which is a big limitation. In later exercises you will create much more robust solutions.
+
+The send_message() function has already been defined for you, as well as the bot_template and user_template variables.
